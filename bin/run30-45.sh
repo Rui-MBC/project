2304ffaf0972e765a4cbc660df4a2178c14530d2
ThePortugueseMan <cemartins99@gmail.com>
2022-06-20 14:45:07 +0100

    testfiles

---
+++ bin/run30-45.sh
@@ -0,0 +1,22 @@
+clear
+javac main/*.java
+javac gamePlay/*.java
+javac cards/*.java
+jar cmf manif.txt Video.jar main/* gamePlay/* cards/*
+jar tf Video.jar
+java -jar Video.jar -d 1000 cmd.comand 30.deck
+java -jar Video.jar -d 1000 cmd.comand 31.deck
+java -jar Video.jar -d 1000 cmd.comand 32.deck
+java -jar Video.jar -d 1000 cmd.comand 33.deck
+java -jar Video.jar -d 1000 cmd.comand 34.deck
+java -jar Video.jar -d 1000 cmd.comand 35.deck
+java -jar Video.jar -d 1000 cmd.comand 36.deck
+java -jar Video.jar -d 1000 cmd.comand 37.deck
+java -jar Video.jar -d 1000 cmd.comand 38.deck
+java -jar Video.jar -d 1000 cmd.comand 39.deck
+java -jar Video.jar -d 1000 cmd.comand 40.deck
+java -jar Video.jar -d 1000 cmd.comand 41.deck
+java -jar Video.jar -d 1000 cmd.comand 42.deck
+java -jar Video.jar -d 1000 cmd.comand 43.deck
+java -jar Video.jar -d 1000 cmd.comand 44.deck
+java -jar Video.jar -d 1000 cmd.comand 45.deck
