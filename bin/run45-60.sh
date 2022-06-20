d25f52a828ecb59ceed698fe8ecbfa1494f05acf
ThePortugueseMan <cemartins99@gmail.com>
2022-06-20 16:59:59 +0100

    workpls

---
+++ bin/run45-60.sh
@@ -0,0 +1,22 @@
+clear
+javac main/*.java
+javac gamePlay/*.java
+javac cards/*.java
+jar cmf manif.txt Video.jar main/* gamePlay/* cards/*
+jar tf Video.jar
+java -jar Video.jar -d 1000 cmd.comand 45.deck
+java -jar Video.jar -d 1000 cmd.comand 46.deck
+java -jar Video.jar -d 1000 cmd.comand 47.deck
+java -jar Video.jar -d 1000 cmd.comand 48.deck
+java -jar Video.jar -d 1000 cmd.comand 49.deck
+java -jar Video.jar -d 1000 cmd.comand 50.deck
+java -jar Video.jar -d 1000 cmd.comand 51.deck
+java -jar Video.jar -d 1000 cmd.comand 52.deck
+java -jar Video.jar -d 1000 cmd.comand 53.deck
+java -jar Video.jar -d 1000 cmd.comand 54.deck
+java -jar Video.jar -d 1000 cmd.comand 55.deck
+java -jar Video.jar -d 1000 cmd.comand 56.deck
+java -jar Video.jar -d 1000 cmd.comand 57.deck
+java -jar Video.jar -d 1000 cmd.comand 58.deck
+java -jar Video.jar -d 1000 cmd.comand 59.deck
+java -jar Video.jar -d 1000 cmd.comand 60.deck
